d88433902db7f687c43732f974c981a0650b2f36
youssef20886 <73967591+youssef20886@users.noreply.github.com>
2022-10-15 15:23:09 +0100

    barbchou

--- Assets/Scripts/Translation.cs
+++ Assets/Scripts/Translation.cs
@@ -5,72 +5,103 @@ using System;
 
 public class Translation : MonoBehaviour
 {
-    /*
+    
     public GameObject waypoints;
     public float vmax;
+    public float tau;
+    public int i_slow;
     int i;
-    GameObject a;
     GameObject b;
     Vector3 v;
     Vector3 p;
     Vector3 direction;
+    Quaternion rotangle;
     public float margeDeCollision;
+    bool canSlow;
+    float currentV;
+
 
 
     void Start()
     {
+        i_slow = 0;
+        currentV = vmax;
         i = 0;
-        a = waypoints.transform.GetChild(i).gameObject;
-        b = waypoints.transform.GetChild(i + 1).gameObject;
+        b = waypoints.transform.GetChild(i).gameObject;
     }
 
     void Update()
     {
 
         TranslationHandler();
+        RotationHandler();
 
     }
     public void TranslationHandler()
     {
-        direction = b.transform.position - a.transform.position;
+        direction = b.transform.position - transform.position;
         direction.z = 0;
         v = direction.normalized;
         p = transform.position;
-        p += v * vmax * Time.deltaTime;
-
-        transform.position = p;
-
         if (Math.Abs(Vector3.Distance(b.transform.position, p)) < margeDeCollision)
         {
-            //Debug.Log((Vector3.Distance(b.transform.position, p)));
-            if ((i <= waypoints.transform.childCount - 2))
+            slowDown();
+            
+            if ((i <= waypoints.transform.childCount - 1))
             {
-                Debug.Log(i);
-                a = waypoints.transform.GetChild(i).gameObject;
-                b = waypoints.transform.GetChild(i + 1).gameObject;
+                b = waypoints.transform.GetChild(i).gameObject;
 
             }
             else
             {
 
-                a = waypoints.transform.GetChild(waypoints.transform.childCount - 1).gameObject;
                 b = waypoints.transform.GetChild(0).gameObject;
                 i = -1;
             }
             i++;
-            RotationHandler();
+        }
+        else if (i_slow > 0 && waypoints.transform.GetChild(i + 1).CompareTag("End"))
+        {
+             i_slow = 0;
+             StartCoroutine(Delay());
+        }
+        //Ralentissement si en zone critique
+        //if (direction.magnitude <= margeDeCollision )
+        //{
+        //    //currentV = Mathf.Lerp(vmax/4, vmax, Time.deltaTime * 2.0f);
+        //    canSlow = true;
+        //}
+        //else if (canSlow)
+        //{
+        //    //currentV = Mathf.Lerp(vmax, vmax/4, Time.deltaTime * 2.0f);
+        //    canSlow = false;
+        //}
+        p += v * vmax * Time.deltaTime;
+        transform.position = p;
+    }
+
+    public IEnumerator Delay()
+    {
+        Debug.Log("AAAA");
+        yield return new WaitForSeconds(0.3f);
+        vmax *= 3;
+    }
+    public void slowDown()
+    {
+        if (i_slow == 0)
+        {
+            vmax = (vmax / 3);
+            i_slow++;
         }
     }
 
     public void RotationHandler()
     {
 
-
-        Vector3 myDirection = (b.transform.position - transform.position);
-        var angle = Mathf.Atan2(myDirection.y, myDirection.x) * Mathf.Rad2Deg;
-        Quaternion rotation = Quaternion.AngleAxis(angle -90, Vector3.forward);
+        float betadegree = Mathf.Atan2(direction.y, direction.x)*Mathf.Rad2Deg - 90;
+        rotangle = Quaternion.AngleAxis(betadegree, new Vector3(0, 0, 1));
 
 
-        transform.rotation = rotation;
-    }*/
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotangle, Time.deltaTime*3.5f);
+    }
 }
--- Assets/Scripts/Translation1.cs
+++ Assets/Scripts/Translation1.cs
@@ -17,6 +17,7 @@ public class Translation1 : MonoBehaviour
     public float margeDeCollision;
     bool b_slowDown ;
     int i_slow = 0;
+    Quaternion rotangle;
 
 
     void Start()
@@ -78,19 +79,29 @@ public class Translation1 : MonoBehaviour
     public void RotationHandler()
     {
         Vector3 Direction = (b.transform.position - transform.position).normalized;
-        
-        var angle = Vector3.Angle(transform.up,Direction );      
 
-        //Debug.Log(angle);
-        var angleRad = -angle * Math.PI/180;
-        
-        Quaternion q = new Quaternion(0, 0, (float)Math.Sin(angleRad / 2), 0);
-        Debug.Log(q);
-        //transform.Rotate(new Vector3(0, 0, angle));
-        transform.rotation = Quaternion.Lerp(transform.rotation, q, Time.deltaTime*2f);
-        //transform.Rotate(new Vector3(0, 0, angle));
 
-        //Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        float AngleRad = Mathf.Atan2(b.transform.position.y - transform.position.y, b.transform.position.x - transform.position.x);
+        float AngleDeg = (180 / Mathf.PI) * AngleRad;
+        rotangle = Quaternion.AngleAxis(AngleDeg, new Vector3(0, 0, 1));
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotangle, Time.deltaTime*2.0f);
+
+
+        // TRASH
+        //float betadegree = Vector3.Angle(transform.up, Direction)*Mathf.Rad2Deg;
+        //var angle = Vector3.Angle(transform.up,Direction );      
+
+        ////Debug.Log(angle);
+        //var angleRad = -angle * Math.PI/180;
+
+        //Quaternion q = new Quaternion(0, 0, (float)Math.Sin(angleRad / 2), 0);
+        //Debug.Log(q);
+        ////transform.Rotate(new Vector3(0, 0, angle));
+        //transform.rotation = Quaternion.Lerp(transform.rotation, q, Time.deltaTime*2f);
+        ////transform.Rotate(new Vector3(0, 0, angle));
+
+        ////Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
     }
 
     public void slowDown()
